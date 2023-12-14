f56ae131711319990295c81f148b8d636daa6eeb
kimdohyung <pdohyung@gmail.com>
2023-12-14 19:56:39 +0900

    feat(PairMatchingController): 과정, 레벨, 미션을 입력받고 페어 매칭 결과를 보여준다.

---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,63 @@
+package pairmatching.controller;
+
+import pairmatching.domain.MatchingOption;
+import pairmatching.repository.PairMatchingRepository;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+import java.util.List;
+
+public class PairMatchingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final PairMatchingRepository pairMatchingRepository;
+
+    public PairMatchingController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.pairMatchingRepository = new PairMatchingRepository();
+    }
+
+    public void run() {
+        while (true) {
+            String chooseFunction = retryChooseFunction();
+            if (chooseFunction.equals("Q")) {
+                break;
+            }
+            performFunction(chooseFunction);
+        }
+    }
+
+    private void performFunction(String chooseFunction) {
+        if (chooseFunction.equals("1")) {
+            matchingPair();
+        }
+    }
+
+    private void matchingPair() {
+        MatchingOption matchingOption = retryMatchingOption();
+        List<String> pairMatchingResult = pairMatchingRepository.createPairMatching(matchingOption);
+        outputView.printPairMatchingResult(pairMatchingResult);
+        return;
+    }
+
+    private String retryChooseFunction() {
+        while (true) {
+            try {
+                return inputView.inputChooseFunction();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private MatchingOption retryMatchingOption() {
+        while (true) {
+            try {
+                return inputView.inputMatchingOption();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+}
