d6f26fd13db02f6029495a402324bfcc38343700
kimdohyung <pdohyung@gmail.com>
2023-12-14 19:53:11 +0900

    feat(ShuffleMachine): 크루원의 이름을 섞어주는 기능 추가

---
+++ src/main/java/pairmatching/domain/ShuffleMachine.java
@@ -0,0 +1,62 @@
+package pairmatching.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import pairmatching.domain.enums.Course;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static pairmatching.domain.enums.Course.*;
+
+public class ShuffleMachine {
+    private final List<String> crews;
+
+    public ShuffleMachine(Course course) {
+        this.crews = createCrews(course);
+    }
+
+    public List<String> getCrews() {
+        return crews;
+    }
+
+    private List<String> createCrews(Course course) {
+        if (course.equals(BACKEND)) {
+            return createFrontCrews();
+        }
+        return createBackendCrews();
+    }
+
+    private List<String> createFrontCrews() {
+        List<String> frontCrews = new ArrayList<>();
+        try {
+            File frontendCrews = new File("src/main/resources/frontend-crew.md");
+            BufferedReader frontendCrewsReader = new BufferedReader(new FileReader(frontendCrews));
+            String frontendCrew;
+            while ((frontendCrew = frontendCrewsReader.readLine()) != null) {
+                frontCrews.add(frontendCrew);
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        return Randoms.shuffle(frontCrews);
+    }
+
+    private List<String> createBackendCrews() {
+        List<String> backCrews = new ArrayList<>();
+        try {
+            File frontendCrews = new File("src/main/resources/frontend-crew.md");
+            BufferedReader frontendCrewsReader = new BufferedReader(new FileReader(frontendCrews));
+            String frontendCrew;
+            while ((frontendCrew = frontendCrewsReader.readLine()) != null) {
+                backCrews.add(frontendCrew);
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        return Randoms.shuffle(backCrews);
+    }
+}
