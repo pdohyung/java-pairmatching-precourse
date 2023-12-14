202d7afad0d99bdf2c239c65476c5d4f72d24586
kimdohyung <pdohyung@gmail.com>
2023-12-14 18:21:02 +0900

    feat(OutputView): `[ERROR]`로 시작하는 에러메시지를 출력한다.

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,6 +1,9 @@
 package pairmatching.view;
 
+import static java.text.MessageFormat.*;
+
 public class OutputView {
+    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] {0}";
     private static final String PRINT_COURSE_AND_MISSION_MESSAGE = "#############################################\n" +
             "과정: 백엔드 | 프론트엔드\n" +
             "미션:\n" +
@@ -9,9 +12,11 @@ public class OutputView {
             "  - 레벨3: \n" +
             "  - 레벨4: 성능개선 | 배포\n" +
             "  - 레벨5: \n" +
-            "############################################\n" +
-            "과정, 레벨, 미션을 선택하세요.\n" +
-            "ex) 백엔드, 레벨1, 자동차경주";
+            "############################################\n";
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(format(ERROR_MESSAGE_FORMAT, errorMessage));
+    }
 
     public void printCourseAndMission() {
         System.out.println(PRINT_COURSE_AND_MISSION_MESSAGE);
